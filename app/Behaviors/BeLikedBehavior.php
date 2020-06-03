ecede551bf0103464d073f55dddedd5914ad44b2
SebastianKennedy <sebastiankennedy@foxmail.com>
2020-06-03 11:34:33 +0800

    Test Package

--- app/Behaviors/BeLikedBehavior.php
+++ app/Behaviors/BeLikedBehavior.php
@@ -6,17 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 trait BeLikedBehavior
 {
-    public function isLikedBy(Model $model)
+    public function isLikedBy(Model $user)
     {
-        if (is_a($model, config('auth.providers.users.model'))) {
+        if (is_a($user, config('auth.providers.users.model'))) {
             if ($this->relationLoaded('likers')) {
-                return $this->likers->contains($model);
+                return $this->likers->contains($user);
             }
 
             if ($this->relationLoaded('likes')) {
-                return $this->likes->where(config('like.foreign_key'), $model->getKey())->count() > 0;
+                return $this->likes->where(config('like.foreign_key'), $user->getKey())->count() > 0;
             }
 
+            $like_model_name = config('like.model');
+            $like_model = new $like_model_name();
+
+            return $like_model::query()
+                    ->where(config('like.morph_many_id'), $this->getKey())
+                    ->where(config('like.morph_many_type'), $this->getMorphClass())
+                    ->where(config('like.foreign_key'), $user->getKey())
+                    ->count() > 0;
         }
 
         return false;
@@ -31,10 +39,9 @@ trait BeLikedBehavior
     {
         return $this->belongsToMany(
             config('auth.providers.users.model'),
-            config('like.table'),
-            config('like.morph_many_name') . '_id',
+            config('like.table_name'),
+            config('like.morph_many_id'),
             config('like.foreign_key')
-        )
-            ->where(config('like.morph_many_name') . '_type', $this->getMorphClass());
+        )->where('likeable_type', $this->getMorphClass());
     }
 }
--- app/Behaviors/ToLikeBehavior.php
+++ app/Behaviors/ToLikeBehavior.php
@@ -2,7 +2,31 @@
 
 namespace App\Behaviors;
 
+use Illuminate\Database\Eloquent\Model;
+
 trait ToLikeBehavior
 {
+    public function hasLiked(Model $model)
+    {
+        if ($this->relationLoaded('likes')) {
+            return $this->likes
+                    ->where(config('like.morph_many_id'), $model->getKey())
+                    ->where(config('like.morph_many_type'), $model->getMorphClass())
+                    ->count() > 0;
+        }
 
+        $like_model_name = config('like.model');
+        $like_model = new $like_model_name();
+
+        return $like_model::query()
+                ->where(config('like.foreign_key'), $this->getKey())
+                ->where(config('like.morph_many_id'), $model->getKey())
+                ->where(config('like.morph_many_type'), $model->getMorphClass())
+                ->count() > 0;
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(config('like.model'), config('like.foreign_key'), $this->getKeyName());
+    }
 }
